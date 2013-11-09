fc49d07831750e484fb7bc832f5acaafaee41672
chaihf <chaihefulovese@gmail.com>
2013-11-09 17:40:06 -0500

    nameNodeAPI

--- src/HDFS_NameNode/NameNodeAPI.java
+++ src/HDFS_NameNode/NameNodeAPI.java
@@ -1,5 +1,23 @@
 package HDFS_NameNode;
 
-public class NameNodeAPI {
+import java.rmi.RemoteException;
+import java.rmi.server.UnicastRemoteObject;
 
+import MetaData.DataNodeMeta;
+import MetaData.FileMeta;
+
+public class NameNodeAPI extends UnicastRemoteObject implements NameNodeAPIInterface{
+	public NameNodeAPI() throws RemoteException {
+		
+	}
+	
+	public FileMeta GetFileInfo(String fileName) throws RemoteException {
+		return new FileMeta();
+	}
+	public DataNodeMeta GetNodeInfo(String nodeName) throws RemoteException {
+		return new DataNodeMeta();
+	}
+	public Boolean TransferFile(String fileName, String DataNode1, String DataNode2) throws RemoteException {
+		return true;
+	}
 }
