d168864b48fbc3daae281dd2a9e083857a9daa4a
slaviaguns <zhws1990@gmail.com>
2013-11-09 22:08:34 -0500

    aaaaa

--- src/HDFS_DataNode/ProcessDataInterface.java
+++ src/HDFS_DataNode/ProcessDataInterface.java
@@ -2,7 +2,10 @@ package HDFS_DataNode;
 
 import java.rmi.Remote;
 
+import MetaData.DataNodeMeta;
+
 public interface ProcessDataInterface extends Remote {
 	public void PlaceSplittedFile(String filename, byte[] filecontent);
 	public void TransferSplittedFile(String fromfilename, String tofilename, String datanodename);
+	public DataNodeMeta SendHeartBeat();
 }
