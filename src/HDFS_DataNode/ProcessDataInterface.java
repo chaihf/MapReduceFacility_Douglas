9fa4faffee39ce7845d220915ccbc9ac0ebc809c
chaihf <chaihefulovese@gmail.com>
2013-11-09 21:33:04 -0500

    Management Tool

--- src/HDFS_DataNode/ProcessDataInterface.java
+++ src/HDFS_DataNode/ProcessDataInterface.java
@@ -1,8 +1,10 @@
 package HDFS_DataNode;
 
 import java.rmi.Remote;
+import java.util.ArrayList;
 
 public interface ProcessDataInterface extends Remote {
 	public void PlaceSplittedFile(String filename, byte[] filecontent);
 	public byte[] GetSplittedFile(String filename);
+	
 }
--- src/HDFS_NameNode/NameNodeAPIInterface.java
+++ src/HDFS_NameNode/NameNodeAPIInterface.java
@@ -2,12 +2,15 @@ package HDFS_NameNode;
 
 import java.rmi.Remote;
 import java.rmi.RemoteException;
+import java.util.ArrayList;
+
 import MetaData.*;
 
 public interface NameNodeAPIInterface extends Remote{
 	public FileMeta GetFileInfo(String fileName) throws RemoteException;
 	public DataNodeMeta GetNodeInfo(String nodeName) throws RemoteException;
 	public Boolean TransferFile(String fileName, String DataNode1, String DataNode2) throws RemoteException;
+	public ArrayList<String> GetDataNodeRMI();
 //	public FileMeta GetFileInfo(String name) throws RemoteException;
 
 }
